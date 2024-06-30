af8442c5959603ed1b9a648506e89bb2a5d79e6b
王忆嘉 <1799575127@qq.com>
2024-06-30 16:23:51 +0800

    初始化

---
+++ src/main/java/com/example/dangerbehaviordetect/Mapper/VCodeMapper.java
@@ -0,0 +1,19 @@
+package com.example.dangerbehaviordetect.Mapper;
+
+import com.example.dangerbehaviordetect.pojo.VCode;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Mapper
+public interface VCodeMapper {
+
+    @Insert("insert into ValidateCode(mail, code,cTime) VALUES(#{mail}, #{code},#{cTime})")
+    public void insert(String mail, String code, LocalDateTime cTime);
+
+    @Select("select * from ValidateCode where mail = #{mail} and code = #{code}")
+    public List<VCode> getCode(String mail, String code);
+}

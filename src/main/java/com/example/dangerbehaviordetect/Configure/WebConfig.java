fdd8f099de8155b1ccb91bbd34ae56cf6a0ff31c
linx <2737573463@qq.com>
2024-07-04 16:31:06 +0800

    修改logparam

--- src/main/java/com/example/dangerbehaviordetect/Configure/WebConfig.java
+++ src/main/java/com/example/dangerbehaviordetect/Configure/WebConfig.java
@@ -1,6 +1,6 @@
 package com.example.dangerbehaviordetect.Configure;
 
-import com.example.dangerbehaviordetect.pojo.LogParamsInterceptor;
+import com.example.dangerbehaviordetect.interceptor.LogParamsInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
--- src/main/java/com/example/dangerbehaviordetect/interceptor/LogParamsInterceptor.java
+++ src/main/java/com/example/dangerbehaviordetect/interceptor/LogParamsInterceptor.java
@@ -1,4 +1,4 @@
-package com.example.dangerbehaviordetect.pojo;
+package com.example.dangerbehaviordetect.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
